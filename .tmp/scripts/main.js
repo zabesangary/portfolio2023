504202441927ae5fd59c5ab37316fc6ee6557b91
Zabe <zabivirdee@gmail.com>
2017-07-25 15:10:33 +0200

    More files

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -173,11 +173,11 @@ $(document).ready(function () {
 		$('.hover-it7').fadeToggle('slow').css('visibility', 'visible');
 	});
 });
-$("#contactForm").validator().on("submit", function (event) {
+$('#contactForm').validator().on('submit', function (event) {
 	if (event.isDefaultPrevented()) {
 		// handle the invalid form...
 		formError();
-		submitMSG(false, "Did you fill in the form properly?");
+		submitMSG(false, 'Did you fill in the form properly?');
 	} else {
 		// everything looks good!
 		event.preventDefault();
@@ -187,17 +187,17 @@ $("#contactForm").validator().on("submit", function (event) {
 
 function submitForm() {
 	// Initiate Variables With Form Content
-	var name = $("#name").val();
-	var email = $("#email").val();
-	var msg_subject = $("#msg_subject").val();
-	var message = $("#message").val();
+	var name = $('#name').val();
+	var email = $('#email').val();
+	var msg_subject = $('#msg_subject').val();
+	var message = $('#message').val();
 
 	$.ajax({
-		type: "POST",
-		url: "assets/php/form-process.php",
-		data: "name=" + name + "&email=" + email + "&msg_subject=" + msg_subject + "&message=" + message,
+		type: 'POST',
+		url: 'assets/php/form-process.php',
+		data: 'name=' + name + '&email=' + email + '&msg_subject=' + msg_subject + '&message=' + message,
 		success: function (text) {
-			if (text == "success") {
+			if (text == 'success') {
 				formSuccess();
 			} else {
 				formError();
@@ -208,22 +208,22 @@ function submitForm() {
 }
 
 function formSuccess() {
-	$("#contactForm")[0].reset();
-	submitMSG(true, "Message Submitted!");
+	$('#contactForm')[0].reset();
+	submitMSG(true, 'Message Submitted!');
 }
 
 function formError() {
-	$("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+	$('#contactForm').removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
 		$(this).removeClass();
 	});
 }
 
 function submitMSG(valid, msg) {
 	if (valid) {
-		var msgClasses = "h3 text-center tada animated text-success";
+		var msgClasses = 'h3 text-center tada animated text-success';
 	} else {
-		var msgClasses = "h3 text-center text-danger";
+		var msgClasses = 'h3 text-center text-danger';
 	}
-	$("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+	$('#msgSubmit').removeClass().addClass(msgClasses).text(msg);
 }
 //# sourceMappingURL=main.js.map
